8e50853e57487207bd91851f29d5ef6e5eff3f80
ctrlaltaf <altafayyubibarelvi@gmail.com>
2024-08-27 16:26:37 -0700

    added emojis

--- client/src/Components/Envelope/Envelope.tsx
+++ client/src/Components/Envelope/Envelope.tsx
@@ -5,13 +5,14 @@ import { animate, motion, Variants } from "framer-motion";
 const Envelope = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [buttonX, setButtonX] = useState(0);
+    const [emoji, setEmoji] = useState("📦");
 
     const itemVariants: Variants = {
         open: {
             opacity: 1,
             y: 0,
-            transition: { type: "spring", stiffness: 50, damping: 30 },
-            x: -100,
+            transition: { type: "spring", stiffness: 100, damping: 20 },
+            x: 0,
         },
         closed: { opacity: 0, y: 20, transition: { duration: 0.2 } },
     };
@@ -27,14 +28,19 @@ const Envelope = () => {
                     whileHover={{ scale: 1.2 }}
                     whileTap={{ scale: 0.97 }}
                     animate={{ x: buttonX }}
+                    className="envelope-button"
                     onClick={() => {
                         if (isOpen) {
                             setButtonX(buttonX + 200);
-                        } else setButtonX(buttonX -200);
+                            setEmoji("📦")
+                        } else {
+                            setButtonX(buttonX - 200);
+                            setEmoji("✅")
+                        }
                         setIsOpen(!isOpen);
                     }}
                 >
-                    Area of Interest{" "}
+                    {emoji} Computational Biology{" "}
                 </motion.button>
                 <motion.div
                     className="card-container"
@@ -63,22 +69,92 @@ const Envelope = () => {
                     style={{ pointerEvents: isOpen ? "auto" : "none" }}
                 >
                     <motion.ul className="column-container">
-                        <motion.li variants={itemVariants}>
-                            Description
-                            <p>blah blah blah bah</p>
-                        </motion.li>
-                        <motion.li variants={itemVariants}>
-                            Skills
-                            <motion.ul>
+                        <ul>
+                            <h3>Description</h3>
+                            <motion.li
+                                variants={itemVariants}
+                                className="description-column"
+                            >
+                                I am a computational biology researcher
+                                specializing in developing{" "}
+                                <b>full-stack tools</b> for visualizing protein
+                                and regulatory networks across species. I use{" "}
+                                <b>Python</b> and libraries like{" "}
+                                <b>
+                                    Matplotlib, NetworkX, and Pandas for data
+                                    analysis and benchmarking{" "}
+                                </b>
+                                , and I utilize <b>RStudio</b> for handling
+                                large datasets. My aim is to improve the
+                                understanding of biological networks and create
+                                user-friendly visualization tools for
+                                biologists.
+                            </motion.li>
+                        </ul>
+                        <ul className="skills-column">
+                            <h3>Skills</h3>
+                            <motion.li
+                                variants={itemVariants}
+                                className="content-skills-item"
+                            >
                                 {" "}
-                                <motion.li variants={itemVariants}>
-                                    Skill 1
-                                </motion.li>
-                                <motion.li variants={itemVariants}>
-                                    Skill 2
-                                </motion.li>
-                            </motion.ul>
-                        </motion.li>
+                                <img
+                                    alt="Static Badge"
+                                    src="https://img.shields.io/badge/React-React?logo=react&logoColor=white&labelColor=%2361dbfb&color=%2361dbfb"
+                                    height="25"
+                                />
+                            </motion.li>
+                            <motion.li
+                                variants={itemVariants}
+                                className="content-skills-item"
+                            >
+                                <img
+                                    alt="Static Badge"
+                                    src="https://img.shields.io/badge/Express-Express?logo=express&logoColor=303030&labelColor=68A063&color=68A063"
+                                    height="25"
+                                />
+                            </motion.li>
+                            <motion.li
+                                variants={itemVariants}
+                                className="content-skills-item"
+                            >
+                                <img
+                                    alt="Static Badge"
+                                    src="https://img.shields.io/badge/Neo4j-Neo4j?logo=neo4j&logoColor=%23014063&color=%23FCF9F6"
+                                    height="25"
+                                />
+                            </motion.li>
+                            <motion.li
+                                variants={itemVariants}
+                                className="content-skills-item"
+                            >
+                                <img
+                                    alt="Static Badge"
+                                    src="https://img.shields.io/badge/NGINX-NGINX?logo=nginx&logoColor=green&color=black"
+                                    height="25"
+                                />
+                            </motion.li>
+                            <motion.li
+                                variants={itemVariants}
+                                className="content-skills-item"
+                            >
+                                <img
+                                    alt="Static Badge"
+                                    src="https://img.shields.io/badge/RStudio-RStudio?logo=r&color=4AA4DE"
+                                    height="25"
+                                />
+                            </motion.li>
+                            <motion.li
+                                variants={itemVariants}
+                                className="content-skills-item"
+                            >
+                                <img
+                                    alt="Static Badge"
+                                    src="https://img.shields.io/badge/Python-Python?logo=python&logoColor=ffde57&color=4584b6"
+                                    height="25"
+                                />
+                            </motion.li>
+                        </ul>
                     </motion.ul>
                 </motion.div>
             </motion.nav>
--- client/src/Pages/AboutPage/AboutPage.tsx
+++ client/src/Pages/AboutPage/AboutPage.tsx
@@ -14,6 +14,7 @@ const AboutPage = () => {
             <br></br>
             <Envelope></Envelope>
 
+
         </div>
     );
 };
