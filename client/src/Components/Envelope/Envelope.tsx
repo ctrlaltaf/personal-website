e183a597cfe930e65df1d9c37f25d13022780277
ctrlaltaf <altafayyubibarelvi@gmail.com>
2024-08-26 21:42:49 -0700

    Added envelope component

---
+++ client/src/Components/Envelope/Envelope.tsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import "./Envelope.css";
+import { animate, motion, Variants } from "framer-motion";
+
+const Envelope = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [buttonX, setButtonX] = useState(450);
+
+    const itemVariants: Variants = {
+        open: {
+            opacity: 1,
+            y: 0,
+            transition: { type: "spring", stiffness: 50, damping: 30 },
+            x: -100,
+        },
+        closed: { opacity: 0, y: 20, transition: { duration: 0.2 } },
+    };
+
+    return (
+        <div className="envelope-container">
+            <motion.nav
+                initial={false}
+                animate={isOpen ? "open" : "closed"}
+                className="menu"
+            >
+                <motion.button
+                    whileHover={{ scale: 1.2 }}
+                    whileTap={{ scale: 0.97 }}
+                    animate={{ x: buttonX }}
+                    onClick={() => {
+                        if (isOpen) {
+                            setButtonX(450);
+                        } else setButtonX(-50);
+                        setIsOpen(!isOpen);
+                    }}
+                >
+                    Area of Interest{" "}
+                </motion.button>
+                <motion.div
+                    className="card-container"
+                    variants={{
+                        open: {
+                            clipPath: "inset(0% 0% 0% 0% round 10px)",
+                            transition: {
+                                type: "spring",
+                                bounce: 0,
+                                duration: 0.7,
+                                delayChildren: 0.3,
+                                staggerChildren: 0.05,
+                            },
+                        },
+                        closed: {
+                            clipPath: "inset(10% 50% 90% 50% round 10px)",
+                            transition: {
+                                type: "spring",
+                                bounce: 0,
+                                duration: 0.3,
+                            },
+                        },
+                    }}
+                    style={{ pointerEvents: isOpen ? "auto" : "none" }}
+                >
+                    <motion.ul className="column-container">
+                        <motion.li variants={itemVariants}>
+                            Description
+                            <p>blah blah blah bah</p>
+                        </motion.li>
+                        <motion.li variants={itemVariants}>
+                            Skills
+                            <motion.ul>
+                                {" "}
+                                <motion.li variants={itemVariants}>
+                                    Skill 1
+                                </motion.li>
+                                <motion.li variants={itemVariants}>
+                                    Skill 2
+                                </motion.li>
+                            </motion.ul>
+                        </motion.li>
+                    </motion.ul>
+                </motion.div>
+            </motion.nav>
+        </div>
+    );
+};
+
+export default Envelope;
--- client/src/Pages/AboutPage/AboutPage.tsx
+++ client/src/Pages/AboutPage/AboutPage.tsx
@@ -1,9 +1,21 @@
+import Envelope from "../../Components/Envelope/Envelope.tsx";
+import MotionTesting from "../../Components/MotionTesting/MotionTesting.tsx";
+import Variant from "../../Components/Variant/Variant.tsx";
+
 const AboutPage = () => {
-  return (
-    <div>
-      <h1>About Page</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>About Page</h1>
+            {/* <MotionTesting></MotionTesting> */}
+            {/* <Variant></Variant> */}
+            <Envelope></Envelope>
+            <br></br>
+            <Envelope></Envelope>
+            <br></br>
+            <Envelope></Envelope>
+
+        </div>
+    );
 };
 
 export default AboutPage;
